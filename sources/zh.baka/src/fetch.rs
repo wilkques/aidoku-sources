8212a61ddcd838a2832debfcfb45fa943b98d44d
wilkques@gmail.com <wilkques@gmail.com>
2026-03-01 15:44:28 +0800

    fix zh.baka cloudflare bypass

--- sources/zh.baka/src/fetch.rs
+++ sources/zh.baka/src/fetch.rs
@@ -2,16 +2,13 @@ use aidoku::{
     Result,
     alloc::String,
     imports::net::{HttpMethod, Request},
-    prelude::*,
 };
-
-use crate::settings::get_base_url;
 
 pub struct Fetch;
 
 impl Fetch {
     pub fn request(url: String, method: HttpMethod) -> Result<Request> {
-        Ok(Request::new(url, method)?.header("Referer", &format!("{}/", get_base_url())))
+        Ok(Request::new(url, method)?)
     }
 
     pub fn get(url: String) -> Result<Request> {
